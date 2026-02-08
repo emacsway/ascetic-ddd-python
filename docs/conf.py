d0380024c63f4889276e7125258172bd18e6d5eb
Ivan Zakrevsky <ivzak@yandex.ru>
2026-02-09 00:50:17 +0300

    Fix copyright

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 project = 'ascetic-ddd'
-copyright = '2024, Ivan Zakrevsky'
+copyright = '2026, Ivan Zakrevsky'
 author = 'Ivan Zakrevsky'
 release = '0.1.5'
 
