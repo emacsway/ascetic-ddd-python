987d49638a741c7bbf7f4fb34219e4805c1c6429
Ivan Zakrevsky <ivzak@yandex.ru>
2026-02-16 19:04:12 +0300

    Change Mediator interface

--- ascetic_ddd/mediator/interfaces.py
+++ ascetic_ddd/mediator/interfaces.py
@@ -4,66 +4,81 @@ from abc import ABCMeta, abstractmethod
 from ascetic_ddd.disposable.interfaces import IDisposable
 
 __all__ = (
+    "IRequest",
     "IMediator",
-    "ICommandHandler",
+    "IRequestHandler",
     "IEventHandler",
     "IPipelineHandler",
-    "CommandResultT",
 )
 
+SessionT = typing.TypeVar("SessionT")
+RequestT = typing.TypeVar("RequestT")
+EventT = typing.TypeVar("EventT")
+ResultT = typing.TypeVar("ResultT")
 
-SessionT_co = typing.TypeVar("SessionT_co", covariant=True)
-CommandT_co = typing.TypeVar("CommandT_co", covariant=True)
-EventT_co = typing.TypeVar("EventT_co", covariant=True)
-CommandResultT = typing.TypeVar("CommandResultT")
+
+class IRequest(typing.Generic[ResultT]):
+    pass
 
 
-class ICommandHandler(typing.Protocol[SessionT_co, CommandT_co]):
-    def __call__(self, session: SessionT_co, command: CommandT_co) -> typing.Any:
+class IRequestHandler(typing.Protocol[SessionT, RequestT, ResultT]):
+    def __call__(self, session: SessionT, request: RequestT) -> ResultT:
         ...
 
 
-class IEventHandler(typing.Protocol[SessionT_co, EventT_co]):
-    def __call__(self, session: SessionT_co, event: EventT_co):
+class IEventHandler(typing.Protocol[SessionT, EventT]):
+    def __call__(self, session: SessionT, event: EventT):
         ...
 
 
-class IPipelineHandler(typing.Protocol[SessionT_co, CommandT_co]):
+class IPipelineHandler(typing.Protocol[SessionT, RequestT, ResultT]):
     @abstractmethod
     async def __call__(
-            self, session: SessionT_co, command: CommandT_co, next_: 'ICommandHandler[SessionT_co, CommandT_co]'
-    ) -> typing.Any:
+            self, session: SessionT, request: RequestT,
+            next_: typing.Callable[..., typing.Awaitable[ResultT]]
+    ) -> ResultT:
         ...
 
 
-class IMediator(typing.Generic[CommandT_co, EventT_co, SessionT_co], metaclass=ABCMeta):
+class IMediator(typing.Generic[SessionT], metaclass=ABCMeta):
 
     @abstractmethod
-    async def send(self, session: SessionT_co, command: CommandT_co):
+    async def send(self, session: SessionT, request: IRequest[ResultT]) -> ResultT:
         raise NotImplementedError
 
     @abstractmethod
-    async def register(self, command_type: type[CommandT_co], handler: ICommandHandler[SessionT_co, CommandT_co]) -> IDisposable:
-        raise NotImplementedError
-
-    @abstractmethod
-    async def unregister(self, command_type: type[CommandT_co]) -> None:
-        raise NotImplementedError
-
-    @abstractmethod
-    async def publish(self, session: SessionT_co, event: EventT_co) -> None:
-        raise NotImplementedError
-
-    @abstractmethod
-    async def subscribe(
-            self, event_type: type[EventT_co], handler: IEventHandler[SessionT_co, EventT_co], weak: bool = False
+    async def register(
+            self,
+            request_type: type[IRequest[ResultT]],
+            handler: IRequestHandler[SessionT, IRequest[ResultT], ResultT]
     ) -> IDisposable:
         raise NotImplementedError
 
     @abstractmethod
-    async def unsubscribe(self, event_type: type[EventT_co], handler: IEventHandler[SessionT_co, EventT_co]) -> None:
+    async def unregister(self, request_type: type[IRequest[ResultT]]) -> None:
         raise NotImplementedError
 
     @abstractmethod
-    async def add_pipeline(self, pipeline: IPipelineHandler[SessionT_co, CommandT_co]) -> None:
+    async def publish(self, session: SessionT, event: EventT) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def subscribe(
+            self,
+            event_type: type[EventT],
+            handler: IEventHandler[SessionT, EventT],
+            weak: bool = False
+    ) -> IDisposable:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def unsubscribe(self, event_type: type[EventT], handler: IEventHandler[SessionT, EventT]) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def add_pipeline(
+            self,
+            request_type: typing.Optional[type[IRequest[ResultT]]],
+            pipeline: IPipelineHandler[SessionT, IRequest[ResultT], ResultT]
+    ) -> None:
         raise NotImplementedError
