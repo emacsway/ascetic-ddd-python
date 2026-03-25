4132e6677e9945e1ee6db605768ef45de272287d
Ivan Zakrevsky <ivzak@yandex.ru>
2026-03-25 21:18:52 +0300

    Distributors CQRS: remove SequenceDistributor

--- ascetic_ddd/faker/infrastructure/tests/distributors/m2o/test_pg_sequence_distributor.py
+++
@@ -1,9 +0,0 @@
-from ascetic_ddd.faker.domain.tests.distributors.m2o.test_sequence_distributor import SequenceDistributorTestCase
-from ascetic_ddd.faker.infrastructure.distributors.m2o.factory import pg_distributor_factory
-
-
-# logging.basicConfig(level="INFO")
-
-
-class PgSequenceDistributorTestCase(SequenceDistributorTestCase):
-    distributor_factory = staticmethod(pg_distributor_factory)
