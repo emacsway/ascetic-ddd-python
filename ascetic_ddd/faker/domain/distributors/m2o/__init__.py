a80f76ba60a42a87125152007e4d9e88a43dc288
Ivan Zakrevsky <ivzak@yandex.ru>
2026-03-26 00:03:12 +0300

    Distributors CQRS: update package imports

--- ascetic_ddd/faker/domain/distributors/m2o/__init__.py
+++ ascetic_ddd/faker/domain/distributors/m2o/__init__.py
@@ -1,8 +1,8 @@
 from ascetic_ddd.faker.domain.distributors.m2o.interfaces import IM2ODistributor, IM2ODistributorFactory
 from ascetic_ddd.faker.domain.distributors.m2o.dummy_distributor import DummyDistributor
+from ascetic_ddd.faker.domain.distributors.m2o.write_distributor import WriteDistributor
 from ascetic_ddd.faker.domain.distributors.m2o.weighted_distributor import WeightedDistributor
 from ascetic_ddd.faker.domain.distributors.m2o.skew_distributor import SkewDistributor
-from ascetic_ddd.faker.domain.distributors.m2o.sequence_distributor import SequenceDistributor
 from ascetic_ddd.faker.domain.distributors.m2o.nullable_distributor import NullableDistributor
 from ascetic_ddd.faker.domain.distributors.m2o.factory import distributor_factory
 
@@ -10,9 +10,9 @@ __all__ = (
     'IM2ODistributor',
     'IM2ODistributorFactory',
     'DummyDistributor',
+    'WriteDistributor',
     'WeightedDistributor',
     'SkewDistributor',
-    'SequenceDistributor',
     'NullableDistributor',
     'distributor_factory',
 )
