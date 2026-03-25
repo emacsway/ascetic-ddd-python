8d671507db5aadfb0caaec3ded509cb8e6b33322
Ivan Zakrevsky <ivzak@yandex.ru>
2026-03-25 21:08:12 +0300

    Distributors CQRS: update package imports

--- ascetic_ddd/faker/infrastructure/distributors/m2o/__init__.py
+++ ascetic_ddd/faker/infrastructure/distributors/m2o/__init__.py
@@ -1,11 +1,11 @@
 from ascetic_ddd.faker.infrastructure.distributors.m2o.pg_weighted_distributor import PgWeightedDistributor
 from ascetic_ddd.faker.infrastructure.distributors.m2o.pg_skew_distributor import PgSkewDistributor
-from ascetic_ddd.faker.infrastructure.distributors.m2o.pg_sequence_distributor import PgSequenceDistributor
+from ascetic_ddd.faker.infrastructure.distributors.m2o.pg_write_distributor import PgWriteDistributor
 from ascetic_ddd.faker.infrastructure.distributors.m2o.factory import pg_distributor_factory
 
 __all__ = (
     'PgWeightedDistributor',
     'PgSkewDistributor',
-    'PgSequenceDistributor',
+    'PgWriteDistributor',
     'pg_distributor_factory',
 )
